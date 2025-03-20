c1f1063838598b4003dd0b9635da89008618f87a
mahimaaGupta <mahimaguptamng@gmail.com>
2025-03-20 19:45:25 +0530

    Use Command Line Arguments for downloading Data and Add alternate commands for mac

--- data_collection/cellular/fetchdata_script.sh
+++ data_collection/cellular/fetchdata_script.sh
@@ -1,7 +1,7 @@
 # Set start and end dates (YYYYMMDD format)
-START_DATE="20250307"
-END_DATE="20250308"
-NUM_FILES=2  # Number of files to select per day
+START_DATE="$1"
+END_DATE="$2"
+NUM_FILES=$3  # Number of files to select per day
 FILE_LIST="file_paths.txt"  # Output file storing selected file paths
 DATA_DIR="data"  # Directory where files will be downloaded
 
@@ -20,15 +20,21 @@ while [[ "$current_date" -le "$END_DATE" ]]; do
 
     # List all files, shuffle them randomly, then select the configured number of files
     gsutil ls "$GCS_PATH" 2>/dev/null | shuf | head -n "$NUM_FILES" >> "$FILE_LIST"
+    # Alternate to shuf
+    # gsutil ls "$GCS_PATH" 2>/dev/null | awk 'BEGIN {srand()} {print rand(), $0}' | sort -k1,1n | cut -d' ' -f2- | head -n "$NUM_FILES" >> "$FILE_LIST"
+    
 
     # Move to the next day
     current_date=$(date -d "$current_date + 1 day" +"%Y%m%d")
+    # Alternate for mac compatibility
+    # current_date=$(date -j -v+1d -f "%Y%m%d" "$current_date" +"%Y%m%d")
+
 done
 
-# Download all collected files into the specified data directory
+Download all collected files into the specified data directory
 cat "$FILE_LIST" | xargs -I {} -P 10 gsutil -m cp "{}" "$DATA_DIR/"
 
 echo "Download complete!"
 
-# Delete the file list after downloads are complete
+Delete the file list after downloads are complete
 rm -f "$FILE_LIST"
